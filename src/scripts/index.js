b297b768101dff9f9a4bf8b255901835e41d40dc
Muhammad Daffa' A.R <daffavcd@gmail.com>
2022-04-12 16:10:23 +0700

    card prototype done

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -3,5 +3,6 @@ import 'regenerator-runtime'; /* for async await transpile */
 import '../styles/main.css';
 
 import "@fontsource/poppins";
+import "rater-js";
 
-console.log('Hello Codsers! :)');
+console.log('Hello Coders! :)');
