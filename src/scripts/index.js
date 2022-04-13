eb437de40d648e342788b6233466a6c836679cff
Muhammad Daffa' A.R <daffavcd@gmail.com>
2022-04-13 11:55:07 +0700

    calling data from json

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -5,5 +5,41 @@ import '../styles/main.css';
 import '@fontsource/poppins';
 import 'rater-js';
 
+import data_resto from '../DATA.json';
 
-console.log('Hello Coders! :)');
+let html = '';
+data_resto.restaurants.forEach((resto) => {
+  html += `
+  <article class="card">
+  <div class="row relative">
+    <img style="height: 315px;"
+      src="${resto.pictureId}"
+      alt="${resto.name}"
+    />
+    <div class="image-badge">Kota ${resto.city}</div>
+  </div>
+  <div class="card-body">
+    <div class="row inline-block">
+      <div class="left"><h4>${resto.name}</h4></div>
+      <div class="right flex mt-20">
+        <div>
+          <img
+            src="./images/heros/star.png"
+            alt="Star"
+            style="width: 21px"
+          />
+        </div>
+        <font class="b-18">
+          &nbsp${resto.rating}<font class="f-12">/5 </font>
+        </font>
+      </div>
+    </div>
+    <p style="margin-top: -10px">${limit(resto.description, 200)}...</p>
+  </div>
+</article>`;
+  document.getElementById('explore-content').innerHTML = html;
+});
+
+function limit(string = '', limit = 0) {
+  return string.substring(0, limit);
+}
